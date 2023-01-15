0f967a6c03c7d62f6684eb251f7fb34c9f179700
lokesh.k <94343329+lokeshwaran26@users.noreply.github.com>
2023-01-15 17:33:12 +0530

    Create Variable.sol

---
+++ Variable.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Variabes {
+
+    uint256 MyInteger = 10;      //unsigned integer
+    string  MyString  = "Loki";   // String variabels
+    bool    MyBoolean = true;     // Boolean variabels
+    address MyAddress =  '0x12e5'  // Address variabels
+
+
+}
