3bd1f141c4050c7d8960837e80b612e3dd32c48d
lokesh.k <94343329+lokeshwaran26@users.noreply.github.com>
2023-01-15 18:10:54 +0530

    Create Mapping.sol

---
+++ Mapping.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Constructor {
+    address Owner;
+    uint fortune;
+    bool deceased;
+
+    constructor() payable public { // constructor is a special function with runs when the coded is deployed
+        
+        Owner = msg.sender; //msg sender represent address that is being called
+        fortune = msg.value; // msg value tells how much ethers are send
+        deceased = false;
+    }
+     
+    address payable [] familyWallets;
+    
+   // mapping function provide us to map the two data types as key and value
+    mapping(address => uint) inheritance;
+}
